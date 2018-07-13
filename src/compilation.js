f6295cb114251933e3003e9c79bd6ab6278a0d67
Sebastien <sebastien@sbkl.ltd>
2018-07-13 16:21:30 +0800

    add Animated. Add dynamic leading function

--- src/compilation.js
+++ src/compilation.js
@@ -147,9 +147,38 @@ let transformedStyles = Object.keys(styles).reduce((carry, style) => {
 
 }, {});
 
-const mergedStyles = Object.assign(
+let mergedStyles = Object.assign(
     transformedStyles,
     utilities
 );
 
-export default (stylesArray) => stylesArray.map(style => mergedStyles[style]);
+export default (stylesArray) => stylesArray.map(style => {
+
+    if(style.includes('leading')) {
+
+        let prefix = styles.fontSizes.fontSize.prefix;
+
+        let fontSizesKeyArray = Object.keys(options.fontSizes).map(key => prefix + key);
+
+        if(stylesArray.some(style => fontSizesKeyArray.includes(style))) {
+
+            let fontSizeKey = ''
+
+            stylesArray.forEach(style => {
+                if(fontSizesKeyArray.includes(style)) {
+                    fontSizeKey = style.replace(prefix, '');
+                }
+            });
+
+            return {
+                lineHeight: (mergedStyles[style].lineHeight * options.fontSizes[fontSizeKey])
+            }
+        } else {
+            return {
+                lineHeight: mergedStyles[style].lineHeight * 16
+            }
+        }
+    }
+
+    return mergedStyles[style];
+});
