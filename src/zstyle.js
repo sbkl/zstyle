6a1e3227b0c350d293743b20613869fb4c1bea07
Sebastien <sebastien@sbkl.ltd>
2018-07-15 10:42:00 +0800

    added script to generate zstyle.js

---
+++ src/zstyle.js
@@ -0,0 +1,104 @@
+export default {
+    colors: {
+        'orange': '#E38E44',
+        'orange-lightest': '#F4B058',
+        'red': '#FF3860',
+        'white': '#FFFFFF',
+        'black': '#000',
+        'grey-darkest': '#364349',
+        'grey-darker': '#596a73',
+        'grey-dark': '#70818a',
+        'grey': '#9babb4',
+        'grey-light': '#dae4e9',
+        'grey-lighter': '#f3f7f9',
+        'grey-lightest': '#fafcfc',
+        'white': '#ffffff',
+        'transparent': 'transparent'
+    },
+    fontSizes: {
+        'xs': 11,
+        'sm': 13,
+        'base': 16,
+        'lg': 18,
+        'xl': 20,
+        '2xl': 30,
+        '3xl': 40
+    },
+    fontWeights: {
+        'hairline': '100',
+        'thin': '200',
+        'light': '300',
+        'normal': '400',
+        'medium': '500',
+        'semibold': '600',
+        'bold': '700',
+        'extrabold': '800',
+        'black': '900',
+    },
+    borderRadius: {
+        'rounded': 5,
+        'rounded-lg': 10,
+        'rounded-xl': 20,
+        'rounded-full': 999
+    },
+    margin: {
+        '0': 0,
+        '0.5': 5,
+        '1': 10,
+        '2': 20,
+        '3': 30,
+        '4': 40,
+    },
+    padding: {
+        '0': 0,
+        '0.5': 5,
+        '1': 10,
+        '2': 20,
+        '3': 30,
+        '4': 40
+    },
+    width: {
+        '1': 40,
+        '2': 80,
+        '3': 100,
+        '4': 120,
+        '1/2': '50%',
+        '1/3': '33.33%',
+        '1/4': '25%',
+        '2/3': '66.66%',
+        '3/4': '75%',
+        'full': '100%'
+    },
+    height: {
+        '1': 40,
+        '2': 80,
+        '3': 100,
+        '4': 120,
+        '200': 200
+    },
+    borderWidth: {
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4
+    },
+    zIndex: {
+        '1': 10,
+        '2': 20,
+        '3': 30,
+        '4': 40,
+        '5': 50,
+        '888': 888,
+        '999': 999
+    },
+    lineHeight: {
+        'tight': 1,
+        'normal': 1.25,
+        'loose': 1.5
+    },
+    letterSpacing: {
+        'tight': -1,
+        'normal': 0,
+        'wide': 1
+    }
+}
