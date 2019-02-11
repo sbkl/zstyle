b5c5a2d99d967e1e9df2d8bfc17f0ae5e9a3f9bd
Sebastien <sebastien@sbkl.ltd>
2019-02-11 20:35:39 +0800

    add ZTextInput

---
+++ src/components/ZTextInput.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import options from '../../../../zstyle';
+import { TextInput } from 'react-native';
+import compile from '../compilation';
+export default class ZTextInput extends Component {
+    static defaultProps = {
+        zstyle: '',
+        zref: () => {}
+    }
+    componentWillMount() {
+        this._component = {};
+    }
+    componentDidMount() {
+        this.props.zref(this._component);
+    }
+    render() {
+        let {zstyle, style, ...rest} = this.props;
+        let styleArray = zstyle.split(' ');
+        styleArray.forEach(style => options.components[style] && options.components[style].split(' ').forEach(object => styleArray.push(object)));
+        return <TextInput ref={component => this._component = component} style={[compile(styleArray), style]} {...rest}/>
+    }
+}
