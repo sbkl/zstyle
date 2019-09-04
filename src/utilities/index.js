09d23c12939d6297c37a2adcdf8a7aad1ac6bd25
Sebastien <sebastien@sbkl.ltd>
2019-09-05 07:09:33 +0800

    reziseMode

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,10 +1,11 @@
-import flex from './flex';
-import position from './position';
-import overflow from './overflow';
-import text from './text';
-import direction from './direction';
-import display from './display';
+import flex from "./flex"
+import position from "./position"
+import overflow from "./overflow"
+import text from "./text"
+import direction from "./direction"
+import display from "./display"
+import resizeMode from "./resizeMode"
 
 let utilities = {}
 
-export default Object.assign(flex, position, overflow, text, utilities, direction, display);
+export default Object.assign(flex, position, overflow, text, utilities, direction, display, resizeMode)
