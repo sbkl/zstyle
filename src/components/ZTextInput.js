dd971660db1509f2288d95d91c21a0dbffaff68d
Sebastien Koziel <sebastien@sbkl.ltd>
2020-08-07 11:17:43 +0800

    fixed TextInput

--- src/components/ZTextInput.js
+++ src/components/ZTextInput.js
@@ -1,45 +1,51 @@
-import React, { Component } from "react"
-import { TextInput, Dimensions } from "react-native"
-import compile from "../compilation"
+import React, { Component } from "react";
+import { TextInput, Dimensions } from "react-native";
+import compile from "../compilation";
 
 export default class ZTextInput extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			zstyles: {}
-		}
-		this._component = {}
-	}
-	static defaultProps = {
-		zstyle: "",
-		zref: () => {}
-	}
-	componentDidMount() {
-		this.props.zref(this._component)
-		this.compileStyles()
-	}
-	componentDidUpdate(prevProps) {
-		if (prevProps.zstyle != this.props.zstyle) {
-			this.compileStyles()
-		}
-	}
-	onLayout = () => {
-		this.compileStyles()
-	}
-	compileStyles = () => {
-		this.setState({
-			zstyles: compile(this.props.zstyle, Dimensions.get("window").width)
-		})
-	}
-	render() {
-		let { style, ...rest } = this.props
-		return (
-			<TextInput
-				onLayout={this.compileStyles}
-				ref={component => (this._component = component)}
-				style={[this.state.zstyles, style]}
-				{...rest}
-			/>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      zstyles: {},
+    };
+  }
+  static defaultProps = {
+    zstyle: "",
+  };
+  focus = () => {
+    this.textInput.focus();
+  };
+  blur = () => {
+    this.textInput.blur();
+  };
+  componentDidMount() {
+    if (this.props.zref != null) {
+      this.props.zref(this);
+    }
+    this.compileStyles();
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.zstyle != this.props.zstyle) {
+      this.compileStyles();
+    }
+  }
+  onLayout = () => {
+    this.compileStyles();
+  };
+  compileStyles = () => {
+    this.setState({
+      zstyles: compile(this.props.zstyle, Dimensions.get("window").width),
+    });
+  };
+  render() {
+    let { style, ...rest } = this.props;
+    return (
+      <TextInput
+        onLayout={this.compileStyles}
+        ref={(input) => (this.textInput = input)}
+        style={[this.state.zstyles, style]}
+        {...rest}
+      />
+    );
+  }
 }
